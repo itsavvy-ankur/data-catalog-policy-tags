d7875c3690df4267671f7f5b9b8d0bf0929a69e9
Ankur Saxena <iamtheone.ankur@gmail.com>
2021-09-11 23:23:45 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   type        = string
   description = "Project ID"
+  sensitive = true
 }
 
 variable "policy_members" {
   type        = list(any)
   description = "List of IAM policy members"
+  sensitive = true
 }
